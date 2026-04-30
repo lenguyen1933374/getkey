218e809a0ee3c449788a3c0f09c6e72e6e51a210
lenguyen1933374 <114991890+lenguyen1933374@users.noreply.github.com>
2026-05-01 00:26:21 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+const { Telegraf } = require('telegraf');
+const admin = require("firebase-admin");
+const axios = require("axios");
+const serviceAccount = require("./serviceAccount.json");
+
+// Kết nối Firebase
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://adr-8poll-default-rtdb.firebaseio.com"
+});
+
+const db = admin.database();
+const bot = new Telegraf('8419760931:AAFwUzvEaDbobW61aBPahPrcfY164pVY2bU');
+const API_LINK4M = "68d3ec91f1e47945eb523749"; 
+const MY_WEB = "https://lenguyen1933374.github.io/getkey/"; 
+
+bot.command('getkey', async (ctx) => {
+  try {
+    const newKey = "NGUYENMOD-" + Math.random().toString(36).substring(2, 8).toUpperCase();
+    
+    await db.ref("Free_Keys/" + newKey).set({
+      status: "unused",
+      type: "24h",
+      hwid: "",
+      created_at: Date.now()
+    });
+
+    const destination = `${MY_WEB}?key=${newKey}`;
+    const response = await axios.get(`https://link4m.co/api-token?api=${API_LINK4M}&url=${destination}`);
+    
+    ctx.replyWithMarkdown(
+      `*Hệ thống NguyenMod*\n\n` +
+      `👉 [BẤM VÀO ĐÂY ĐỂ VƯỢT LINK LẤY KEY](${response.data.shortenedUrl})\n\n` +
+      `_Vượt link xong sẽ hiện mã để Copy!_`
+    );
+  } catch (e) {
+    ctx.reply("Hệ thống bận, hãy thử lại sau!");
+  }
+});
+
+bot.launch();
+console.log("Bot NguyenMod đang hoạt động...");
